69ad9fb15561ad334e345432eca85f1a8c650fd1
unknown <binglinli106@126.com>
2019-01-01 20:49:58 +0800

    added zhengqi

--- zhengqi/zhengqi_main.py
+++ zhengqi/zhengqi_main.py
@@ -39,6 +39,8 @@ myGBR = GradientBoostingRegressor(alpha=0.9, criterion='friedman_mse', init=None
 
 #myGBR.get_params                 # 获取模型的所有参数
 
+
+
 params = {
         'max_depth':[4],
         'learning_rate': [0.03],
@@ -46,18 +48,28 @@ params = {
         }
 
 
-clf = GridSearchCV(estimator=myGBR, param_grid=params, scoring='neg_mean_squared_error', cv=3)
-clf.fit(train_X, train_Y)
-print('最佳参数：', clf.best_params_)
-print('mse：', - clf.best_score_)
+#clf = GridSearchCV(estimator=myGBR, param_grid=params, scoring='neg_mean_squared_error', cv=3)
+#clf.fit(train_X, train_Y)
+#print('最佳参数：', clf.best_params_)
+#print('mse：', - clf.best_score_)
+#
+#best_GBR = clf.best_estimator_
+#best_GBR.fit(train_X, train_Y)
+#pred_Y = best_GBR.predict(test_X)
+#print(mean_squared_error(test_Y, pred_Y))
 
-best_GBR = clf.best_estimator_
-best_GBR.fit(train_X, train_Y)
-pred_Y = best_GBR.predict(test_X)
-print(mean_squared_error(test_Y, pred_Y))
+cv_params = {'n_estimators': [600, 700]}
+other_params = {'learning_rate': 0.05, 'n_estimators': 500, 'max_depth': 5, 'min_child_weight': 1, 'seed': 0,
+                    'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
 
-Y_pred = best_GBR.predict(test.values)
-results = pd.DataFrame(Y_pred, columns=['target'])
-results.to_csv("results.txt", index=False, header=False)
-print("over")
+model = xgb.XGBRegressor(**other_params)
+myxgb = GridSearchCV(estimator=model, param_grid=cv_params, scoring='neg_mean_squared_error', cv=5, verbose=1)
+myxgb.fit(train_X, train_Y)
+print('参数的最佳取值：{0}'.format(myxgb.best_params_))
+print('最佳模型得分:{0}'.format(-myxgb.best_score_))
+
+#Y_pred = best_GBR.predict(test.values)
+#results = pd.DataFrame(Y_pred, columns=['target'])
+#results.to_csv("results.txt", index=False, header=False)
+#print("over")
 
