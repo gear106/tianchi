cf306a85fb1e9e0a13780f1e97a2081239e01f21
unknown <binglinli106@126.com>
2019-01-01 20:39:48 +0800

    added zhengqi

--- zhengqi/zhengqi_main.py
+++ zhengqi/zhengqi_main.py
@@ -7,7 +7,7 @@ Created on Tue Jan  1 11:45:18 2019
 
 import pandas as pd 
 import numpy as np
-#import xgboost as xgb
+import xgboost as xgb
 
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
@@ -26,19 +26,7 @@ labels = train.target.values
 scaler1 = StandardScaler()
 features = scaler1.fit_transform(features)
 
-train_X, test_X, train_Y, test_Y = train_test_split(features, labels, test_size=0.3, random_state=1)
-
-#myGBR = GradientBoostingRegressor(alpha=0.9, criterion='friedman_mse', init=None,
-#                                  learning_rate=0.03, loss='ls', max_depth=4,
-#                                  max_features='sqrt', max_leaf_nodes=None,
-#                                  min_impurity_decrease=0.0, min_impurity_split=None,
-#                                  min_samples_leaf=1, min_samples_split=2,
-#                                  min_weight_fraction_leaf=0.0, n_estimators=1000,
-#                                  presort='auto', random_state=10, subsample=0.8, verbose=0,
-#                                  warm_start=False)
-myGBR.fit(train_X, train_Y)
-pred_Y = myGBR.predict(test_X)
-print(mean_squared_error(test_Y, pred_Y))
+train_X, test_X, train_Y, test_Y = train_test_split(features, labels, test_size=0.05, random_state=1)
 
 myGBR = GradientBoostingRegressor(alpha=0.9, criterion='friedman_mse', init=None,
                                   learning_rate=0.03, loss='ls', max_depth=4,
@@ -52,8 +40,9 @@ myGBR = GradientBoostingRegressor(alpha=0.9, criterion='friedman_mse', init=None
 #myGBR.get_params                 # 获取模型的所有参数
 
 params = {
-        'max_depth':[4, 8],
-        'n_estimators': [300, 1000]
+        'max_depth':[4],
+        'learning_rate': [0.03],
+        'n_estimators': [1000]
         }
 
 
@@ -67,6 +56,8 @@ best_GBR.fit(train_X, train_Y)
 pred_Y = best_GBR.predict(test_X)
 print(mean_squared_error(test_Y, pred_Y))
 
-
-
+Y_pred = best_GBR.predict(test.values)
+results = pd.DataFrame(Y_pred, columns=['target'])
+results.to_csv("results.txt", index=False, header=False)
+print("over")
 
