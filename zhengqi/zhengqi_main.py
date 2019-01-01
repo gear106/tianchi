1ca60cef07beb6d0566a3551c29c7a4b5a4c8f21
libinglin <binglinli106@126.com>
2019-01-01 19:08:13 +0800

    added aliyun

--- zhengqi/zhengqi_main.py
+++ zhengqi/zhengqi_main.py
@@ -13,6 +13,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error
 
 
 root = 'data/'
@@ -35,23 +36,37 @@ train_X, test_X, train_Y, test_Y = train_test_split(features, labels, test_size=
 #                                  min_weight_fraction_leaf=0.0, n_estimators=1000,
 #                                  presort='auto', random_state=10, subsample=0.8, verbose=0,
 #                                  warm_start=False)
-#myGBR.fit(train_X, train_Y)
-#pred_Y = myGBR.predict(test_X)
-#print(mean_squared_error(test_Y, pred_Y))
+myGBR.fit(train_X, train_Y)
+pred_Y = myGBR.predict(test_X)
+print(mean_squared_error(test_Y, pred_Y))
 
-myGBR = GradientBoostingRegressor()
-myGBR.get_params                 # 获取模型的所有参数
+myGBR = GradientBoostingRegressor(alpha=0.9, criterion='friedman_mse', init=None,
+                                  learning_rate=0.03, loss='ls', max_depth=4,
+                                  max_features='sqrt', max_leaf_nodes=None,
+                                  min_impurity_decrease=0.0, min_impurity_split=None,
+                                  min_samples_leaf=1, min_samples_split=2,
+                                  min_weight_fraction_leaf=0.0, presort='auto', 
+                                  random_state=10, subsample=0.8, verbose=0,
+                                  warm_start=False)
+
+#myGBR.get_params                 # 获取模型的所有参数
 
 params = {
-          'learning_rate': [0.03],
-          'max_features': ['sqrt'],
-          'random_state': [10],
-          'max_depth':[5],
-          'm_estimators': [300, 500, 1000, 1500]
-          }
-clf = GridSearchCV(estimator=myGBR, param_grid=params, scoring='neg_mean_squared_error', cv=5, n_jobs=-1)
+        'max_depth':[4, 8],
+        'n_estimators': [300, 1000]
+        }
+
+
+clf = GridSearchCV(estimator=myGBR, param_grid=params, scoring='neg_mean_squared_error', cv=3)
 clf.fit(train_X, train_Y)
 print('最佳参数：', clf.best_params_)
 print('mse：', - clf.best_score_)
+
+best_GBR = clf.best_estimator_
+best_GBR.fit(train_X, train_Y)
+pred_Y = best_GBR.predict(test_X)
+print(mean_squared_error(test_Y, pred_Y))
+
+
 
 
