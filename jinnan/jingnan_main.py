1e4b0f1aeac65b2b3fc9f50a7831ffe5c83733a7
unknown <binglinli106@126.com>
2019-01-03 16:29:12 +0800

    added qingnan

---
+++ jinnan/jingnan_main.py
@@ -0,0 +1,198 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  2 22:09:19 2019
+
+@author: gear
+github:  https://github.com/gear106
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import jn
+from IPython.display import display, clear_output
+import time
+
+import xgboost as xgb
+from sklearn.linear_model import BayesianRidge
+from scipy import sparse
+import warnings
+import re
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.metrics import mean_squared_error
+
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.svm import SVR
+
+from mlxtend.regressor import StackingCVRegressor
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.filterwarnings("ignore")
+
+root = 'data/'
+
+train = pd.read_csv(root + 'jinnan_round1_train_20181227.csv', encoding='gb18030')
+test = pd.read_csv(root + 'jinnan_round1_testA_20181227.csv', encoding='gb18030')
+
+# 删除类别唯一的特征
+for df in [train, test]:
+    df.drop(['B3', 'B13', 'A13', 'A18', 'A23'], axis=1, inplace=True)
+    
+# 删除缺失率超过90%的列
+good_cols = list(train.columns)
+for col in train.columns:
+    rate = train[col].value_counts(normalize=True, dropna=False).values[0]
+    if rate > 0.9:
+        good_cols.remove(col)
+        
+train = train[good_cols]
+good_cols.remove('收率')
+test  = test[good_cols]
+
+# 删除异常值
+train = train[train['收率'] > 0.85]
+
+# 合并数据集
+target = train['收率']
+del train['收率']
+data = pd.concat([train,test],axis=0,ignore_index=True)
+data = data.fillna(-1)
+
+# 日期中有些输入错误和遗漏
+def t2s(t):
+    try:
+        t,m,s=t.split(":")
+    except:
+        if t=='1900/1/9 7:00':
+            return 7*3600
+        elif t=='1900/1/1 2:30':
+            return 2*3600+30*60
+        elif t==-1:
+            return -1
+        else:
+            return 0
+    
+    try:
+        tm = int(t)*3600+int(m)*60+int(s)
+    except:
+        return 30*60
+    
+    return tm
+for f in ['A5','A7','A9','A11','A14','A16','A24','A26','B5','B7']:
+    data[f] = data[f].apply(t2s)
+
+def getDuration(se):
+    try:
+        sh,sm,eh,em=re.split("[:,-]",se)
+    except:
+        if se=='14::30-15:30':
+            return 3600
+        elif se=='13；00-14:00':
+            return 3600
+        elif se=='21:00-22；00':
+            return 3600
+        elif se=='22"00-0:00':
+            return 7200
+        elif se=='2:00-3;00':
+            return 3600
+        elif se=='1:30-3;00':
+            return 5400
+        elif se=='15:00-1600':
+            return 3600
+        elif se==-1:
+            return -1
+        else:
+            return 30*60
+        
+    try:
+        tm = abs(int(eh)*3600+int(em)*60-int(sm)*60-int(sh)*3600)
+    except:
+        if se=='19:-20:05':
+            return 3600
+        else:
+            return 30*60
+    
+    return tm
+
+for f in ['A20','A28','B4','B9','B10','B11']:
+    data[f] = data.apply(lambda df: getDuration(df[f]), axis=1)
+    
+def sample(s):
+    s, n=s.split("_")
+    
+    return int(n)
+   
+data['样本id'] = data.apply(lambda df: sample(df['样本id']), axis=1)
+    
+    
+    
+#cate_columns = [f for f in data.columns if f != '样本id']
+#data = data[cate_columns]
+scaler1 = StandardScaler().fit(data)
+scaler2 = MinMaxScaler().fit(data)
+
+train_X = scaler2.transform(data[:train.shape[0]])
+test = scaler2.transform(data[train.shape[0]:])
+
+train_Y = target.values
+
+train_X, test_X, train_Y, test_Y = train_test_split(train_X, train_Y, test_size=0.1, random_state=1)
+        
+
+##############################--Ridge--########################################
+ridge = Ridge(alpha=0.01, normalize=True, max_iter=1000, random_state=2019)
+
+###############################--SVR--##########################################
+mySVR = SVR(C=1.8, epsilon=0.2, gamma=0.005)
+
+
+###############################--GBR--##########################################
+myGBR = GradientBoostingRegressor(learning_rate=0.01, loss='ls', n_estimators=1500, max_depth=5,
+                                  max_features='sqrt', max_leaf_nodes=None,
+                                  random_state=10, subsample=0.8, verbose=0,
+                                  warm_start=False)
+
+#myGBR.get_params                 # 获取模型的所有参数
+
+stack = myGBR
+stack.fit(train_X, train_Y)
+Y_pred = stack.predict(test_X)
+print(mean_squared_error(test_Y, Y_pred))
+
+###############################--xgboost--######################################
+
+cv_params = {'n_estimators': [1500]}
+other_params = {'learning_rate': 0.01, 'n_estimators': 500, 'max_depth': 6, 'min_child_weight': 1, 'seed': 0,
+                    'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
+
+model = xgb.XGBRegressor(**other_params)
+mgb = GridSearchCV(estimator=model, param_grid=cv_params, scoring='neg_mean_squared_error', cv=5, verbose=1)
+mgb.fit(train_X, train_Y)
+print('参数的最佳取值：{0}'.format(mgb.best_params_))
+print('最佳模型得分:{0}'.format(-mgb.best_score_))
+myxgb = mgb.best_estimator_
+
+stack = myxgb
+stack.fit(train_X, train_Y)
+Y_pred = stack.predict(test_X)
+print(mean_squared_error(test_Y, Y_pred))
+
+##############################--模型融合--######################################
+stack = StackingCVRegressor(regressors=[myGBR, myxgb], meta_regressor=ridge,
+                             use_features_in_secondary=True, cv=5)
+                
+stack.fit(train_X, train_Y)
+Y_pred = stack.predict(test_X)
+print(mean_squared_error(test_Y, Y_pred))
+pred_Y = stack.predict(test)
+
+sub_df = pd.read_csv(root + 'jinnan_round1_submit_20181227.csv', header=None)
+sub_df[1] = pred_Y
+sub_df[1] = sub_df[1].apply(lambda x:round(x, 3))
+sub_df.to_csv("2019.csv", index=False, header=None)
